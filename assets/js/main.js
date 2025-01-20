f99af10614ac719467d0fe30b94c694e4bdfc63e
thejokers <thejokkers69@gmail.com>
2025-01-20 13:29:03 +0100

    Updated base layout and styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,25 +2,25 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Portfolio loaded successfully!");
 
-  const themeToggle = document.getElementById('theme-toggle');
+  const themeToggle = document.getElementById("theme-toggle");
   const body = document.body;
 
   // Vérifie si un thème est enregistré dans localStorage
-  const savedTheme = localStorage.getItem('theme');
+  const savedTheme = localStorage.getItem("theme");
   if (savedTheme) {
     body.classList.add(savedTheme);
   } else {
-    body.classList.add('dark'); // Mode sombre par défaut
+    body.classList.add("dark"); // Mode sombre par défaut
   }
 
   // Basculer entre le mode clair et sombre
-  themeToggle.addEventListener('click', () => {
-    if (body.classList.contains('dark')) {
-      body.classList.replace('dark', 'light');
-      localStorage.setItem('theme', 'light');
+  themeToggle.addEventListener("click", () => {
+    if (body.classList.contains("dark")) {
+      body.classList.replace("dark", "light");
+      localStorage.setItem("theme", "light");
     } else {
-      body.classList.replace('light', 'dark');
-      localStorage.setItem('theme', 'dark');
+      body.classList.replace("light", "dark");
+      localStorage.setItem("theme", "dark");
     }
   });
 });
